5078a7d77e453f161784e8c98138e810bcc23b5c
Mélanie Pichery <melanie.pichery@laposte.net>
2018-11-15 17:08:45 +0100

    version

--- src/testarray/TestArrayTri.java
+++ src/testarray/TestArrayTri.java
@@ -6,8 +6,10 @@ public class TestArrayTri {
 
 	public static void main(String[] args) {
 		
+		//déclaration et initialisation du tableau
 		int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4} ;
-				
+			
+		    //Tri du tableau avec la méthode sort et affichage du tableau trié
 			Arrays.sort(array);
 			System.out.println(Arrays.toString(array));
 
