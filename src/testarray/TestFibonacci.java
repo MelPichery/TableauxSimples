4e332c82a71069027960d8d3bda5724a91fdcfe6
Mélanie Pichery <melanie.pichery@laposte.net>
2018-11-15 17:29:15 +0100

    version2

--- src/testarray/TestFibonacci.java
+++ src/testarray/TestFibonacci.java
@@ -5,26 +5,29 @@ import java.util.Scanner;
 public class TestFibonacci {
 
 	public static void main(String[] args) {
-		
+		//Déclaration et initialisation de la variable rangn
 		int rangn = 0;
 		
+		//Affichage de la consigne et sauvegarde de rangn
 		Scanner sc1 = new Scanner(System.in);
 		System.out.println("Choisissez un rang N");
 		rangn = sc1.nextInt();	
 		
-		int[] fibonacci = new int[rangn+3];
+		//Création du tableau fibonacci de taille rangn + 1
+		int[] fibonacci = new int[rangn+1];
 		
-		
-		
+		//le tableau prend les valeurs 0 et 1 à l'index 0 et 1
 		fibonacci[0]=0;
 		fibonacci[1]=1;
 		
-		for (int i = 2; i < rangn+3; i++) {
+		//le tableau fibonacci à l'index i prend la valeur de la somme des valeurs de ce même tableau à l'index i-1 et i-2
+		for (int i = 2; i < rangn+1; i++) {
 			
 			fibonacci[i] = fibonacci[i-1]+fibonacci[i-2];
 			
 		}
 		
+		//Affichage du résultat
 		System.out.print("Résultat Suite Fibonacci : "+fibonacci[rangn]+"  ");
 		
 	}
